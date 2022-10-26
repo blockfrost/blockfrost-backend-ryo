998c2953a5d18c60bab09f1238bedafb82fefded
Vladimir Volek <volek.vladimir@gmail.com>
2022-10-26 02:42:04 +0200

    fix: production logger

--- src/server.ts
+++ src/server.ts
@@ -4,19 +4,20 @@ import { getConfig } from './config';
 const port = getConfig().server.port;
 const address = getConfig().server.listenAddress;
 const debug = getConfig().server.debug;
+const logger = {
+  transport:
+    process.env.NODE_ENV === 'development' || debug
+      ? {
+          target: 'pino-pretty',
+          options: {
+            translateTime: 'HH:MM:ss Z',
+          },
+        }
+      : undefined,
+};
 
 const server = app({
-  logger: {
-    transport:
-      process.env.NODE_ENV === 'development' || debug
-        ? {
-            target: 'pino-pretty',
-            options: {
-              translateTime: 'HH:MM:ss Z',
-            },
-          }
-        : undefined,
-  },
+  logger: process.env.NODE_ENV === 'production' ? false : logger,
   ignoreTrailingSlash: true,
 
   // https://www.fastify.io/docs/latest/Server/#maxparamlength
