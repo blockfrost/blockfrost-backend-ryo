adbf0b54b3807fc1ddc64b32a9856218376774c7
Marek Mahut <marek.mahut@fivebinaries.com>
2022-11-19 17:32:40 +0100

    fix(genesis): new preview parameters

--- src/constants/genesis.ts
+++ src/constants/genesis.ts
@@ -32,10 +32,10 @@ export const GENESIS: Record<Network, ResponseTypes.Ledger> = {
     max_lovelace_supply: '45000000000000000',
     network_magic: 2,
     epoch_length: 86_400,
-    system_start: 1_660_003_200,
-    slots_per_kes_period: 86_400,
+    system_start: 1_666_656_000,
+    slots_per_kes_period: 129_600,
     slot_length: 1,
-    max_kes_evolutions: 120,
+    max_kes_evolutions: 62,
     security_param: 432,
   },
   preprod: {
