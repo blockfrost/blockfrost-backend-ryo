bb4b73041ea042f03c932461811f98b7593ee345
slowbackspace <slowbackspace@gmail.com>
2022-12-30 12:01:23 +0100

    fix: object nesting in onchain_metadata with cip68

--- src/routes/assets/index.ts
+++ src/routes/assets/index.ts
@@ -89,7 +89,7 @@ async function assets(fastify: FastifyInstance) {
             const result = validateCIP68Metadata(datumMetadata, referenceNFT.standard);
 
             if (result) {
-              onchainMetadata = datumMetadata;
+              onchainMetadata = datumMetadata?.metadata;
               onchainMetadataStandard = result.version;
             }
           }
