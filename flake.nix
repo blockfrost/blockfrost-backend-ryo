9d57546166b93c2ae7265ed1ed961d3a56a9b63a
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-01-05 16:48:15 +0100

    Nix flake scaffold

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Blockfrost API backend";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {self, nixpkgs}: let
+    lib = nixpkgs.lib;
+    supportedSystems = ["x86_64-linux"]; # lib.systems.flakeExposed;
+    forAllSystems = f: lib.genAttrs supportedSystems (system: f system);
+    legacyPkgs = nixpkgs.legacyPackages;
+    default = lib.genAttrs supportedSystems (system: import ./default.nix {
+      inherit system;
+      pkgs = legacyPkgs.${system};
+    });
+  in {
+    packages = forAllSystems (system: {
+      inherit (default.${system}) blockfrost-backend;
+    });
+    # FIXME checks are not building
+    checks = forAllSystems (system: {
+      inherit (default.${system})
+        blockfrost-backend-test-preview
+        blockfrost-backend-test-mainnet;
+    });
+    overlays.default = self: super: {
+      inherit (self.packages.${super.system}) blockfrost-backend;
+    };
+  };
+}
