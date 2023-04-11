977ade8c75fbcebcf6f786b0d6cb27c17d33d3f5
Richard Marko <srk@48.io>
2023-04-11 13:17:15 +0200

    feat(custom-network): accept byron-genesis.json and shelley-genesis.json

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -20,7 +20,22 @@ export const loadShelleyCustomConfig = (): ResponseTypes.Ledger => {
   try {
     const data = readFileSync(customNetworkConfig.genesisShelleyPath, 'utf8');
 
-    return JSON.parse(data) as ResponseTypes.Ledger;
+    const shelleyGenesis = JSON.parse(data);
+    const dateObject = new Date(shelleyGenesis['systemStart']);
+    const unixTimestamp = Math.floor(dateObject.getTime() / 1000); // millis to seconds
+
+    return {
+      active_slots_coefficient: shelleyGenesis['activeSlotsCoeff'],
+      update_quorum: shelleyGenesis['updateQuorum'],
+      max_lovelace_supply: shelleyGenesis['maxLovelaceSupply'],
+      network_magic: shelleyGenesis['networkMagic'],
+      epoch_length: shelleyGenesis['epochLength'],
+      system_start: unixTimestamp,
+      slots_per_kes_period: shelleyGenesis['slotsPerKESPeriod'],
+      slot_length: shelleyGenesis['slotLength'],
+      max_kes_evolutions: shelleyGenesis['maxKESEvolutions'],
+      security_param: shelleyGenesis['securityParam'],
+    };
   } catch {
     throw new Error(`Could not load custom config file: ${customNetworkConfig.genesisShelleyPath}`);
   }
@@ -33,7 +48,15 @@ export const loadByronCustomConfig = (): ByronEraParameters => {
   try {
     const data = readFileSync(customNetworkConfig.genesisByronPath, 'utf8');
 
-    return JSON.parse(data) as ByronEraParameters;
+    const byronGenesis = JSON.parse(data);
+    const k = byronGenesis['protocolConsts']['k'];
+
+    return {
+      epoch_length: k * 10,
+      slot_length: byronGenesis['blockVersionData']['slotDuration'] / 1000,
+      safe_zone: k * 2,
+      end_epoch: 0,
+    };
   } catch {
     throw new Error(`Could not load custom config file: ${customNetworkConfig.genesisByronPath}`);
   }
