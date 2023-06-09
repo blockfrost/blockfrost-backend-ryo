4531cf0291cdd7c3fef385210cbbe6035ae58f2f
slowbackspace <slowbackspace@gmail.com>
2023-06-09 10:17:09 +0200

    chore: print long diffs

--- vitest.config.integration.ts
+++ vitest.config.integration.ts
@@ -1,5 +1,9 @@
 // eslint-disable-next-line import/extensions
 import { defineConfig } from 'vitest/config';
+import { chai } from 'vitest';
+
+// Allows to print long diffs
+chai.config.truncateThreshold = 100_000;
 
 if (!['mainnet', 'preprod', 'preview'].includes(process.env.NETWORK as string)) {
   throw 'Error NETWORK env variable can be only `mainnet, preview, preprod`';
