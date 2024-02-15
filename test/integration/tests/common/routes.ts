c10d60ff42abf7c77708062e9435bbe9a94f15e5
Vladimir Volek <volek.vladimir@gmail.com>
2024-02-15 14:00:11 +0100

    chore: removed old integration tests

--- test/integration/tests/common/routes.ts
+++
@@ -1,7 +0,0 @@
-import { describe } from 'vitest';
-import { commonFixtures } from '@blockfrost/blockfrost-tests';
-import { generateTestSuite } from '../../utils.js';
-
-describe('Integration Tests - common', () => {
-  generateTestSuite(commonFixtures);
-});
--- test/integration/tests/preview/routes.ts
+++
@@ -1,7 +0,0 @@
-import { describe } from 'vitest';
-import { previewFixtures } from '@blockfrost/blockfrost-tests';
-import { generateTestSuite } from '../../utils.js';
-
-describe('Integration Tests - preview', () => {
-  generateTestSuite(previewFixtures);
-});
