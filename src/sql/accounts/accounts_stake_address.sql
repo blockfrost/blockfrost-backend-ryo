06e23761f7f8f143bdef08749fd85269d2ea25ff
sorki <srk@48.io>
2024-03-31 17:06:09 +0200

    fixup! fixup! fixup! wip: naive insta_rewards

--- src/sql/accounts/accounts_stake_address.sql
+++ src/sql/accounts/accounts_stake_address.sql
@@ -74,16 +74,13 @@ SELECT sa.view AS "stake_address",
      -- SUM of all utxos + withdrawables (rewards (all types including rewards + refunds + treasury + reserves) - withdrawals
   )::TEXT AS "controlled_amount", -- cast to TEXT to avoid number overflow
   (
-    COALESCE(rewards_sum.amount, 0)
-    + COALESCE(instant_rewards_sum.amount, 0)
+    COALESCE(rewards_sum.amount, 0) + COALESCE(instant_rewards_sum.amount, 0)
   )::TEXT AS "rewards_sum", -- cast to TEXT to avoid number overflow
   COALESCE(withdrawals_sum.amount, 0)::TEXT AS "withdrawals_sum", -- cast to TEXT to avoid number overflow
   COALESCE(reserves_sum.amount, 0)::TEXT AS "reserves_sum", -- cast to TEXT to avoid number overflow
   COALESCE(treasury_sum.amount, 0)::TEXT AS "treasury_sum", -- cast to TEXT to avoid number overflow
   (
-    (
-      COALESCE(rewards_sum.amount, 0) + COALESCE(instant_rewards_sum.amount, 0) + COALESCE(refunds_sum.amount, 0) - COALESCE(withdrawals_sum.amount, 0)
-    )
+    COALESCE(rewards_sum.amount, 0) + COALESCE(instant_rewards_sum.amount, 0) + COALESCE(refunds_sum.amount, 0) - COALESCE(withdrawals_sum.amount, 0)
   )::TEXT AS "withdrawable_amount", -- cast to TEXT to avoid number overflow
   (
     SELECT pool_id
@@ -120,7 +117,6 @@ FROM stake_address sa
           SELECT *
           FROM current_epoch
         )
-        AND type <> 'refund'
       )
     GROUP BY addr_id
   ) AS "instant_rewards_sum" ON (instant_rewards_sum.addr_id = sa.id)
