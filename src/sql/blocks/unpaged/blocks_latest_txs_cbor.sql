8bd72aff1e9be6c3c3502f0001df6a3081e62bf7
Vladimir Volek <volek.vladimir@gmail.com>
2025-03-14 15:49:35 +0100

    feat: /blocks/latest/txs/cbor sql

--- src/sql/blocks/unpaged/blocks_latest_txs_cbor.sql
+++ src/sql/blocks/unpaged/blocks_latest_txs_cbor.sql
@@ -1,21 +1,14 @@
-SELECT encode(tx.hash, 'hex') AS "hash"
+SELECT 
+    encode(tx.hash, 'hex') AS tx_hash,
+    encode(tx_cbor.bytes, 'hex') AS cbor
 FROM tx
-WHERE CASE
-    WHEN $1 ~ '^[0-9]+$' THEN block_id = (
-      SELECT id
-      FROM block
-      WHERE block_no = $1::INTEGER
-    )
-    ELSE block_id = (
-      SELECT id
-      FROM block
-      WHERE encode(hash, 'hex') = $1
-    )
-  END
-ORDER BY CASE
-    WHEN LOWER($2) = 'desc' THEN tx.block_index
-  END DESC,
-  CASE
-    WHEN LOWER($2) <> 'desc'
-    OR $2 IS NULL THEN tx.block_index
-  END ASC
+JOIN tx_cbor ON tx.id = tx_cbor.tx_id
+WHERE tx.block_id = (
+    SELECT id
+    FROM block
+    ORDER BY id DESC
+    LIMIT 1
+)
+ORDER BY 
+    CASE WHEN LOWER($1) = 'desc' THEN tx.block_index END DESC,
+    CASE WHEN LOWER($1) <> 'desc' OR $1 IS NULL THEN tx.block_index END ASC;
