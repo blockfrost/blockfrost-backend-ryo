2354bd836fd071d1e2c3988ced88c8478d2df4fa
slowbackspace <slowbackspace@gmail.com>
2025-10-08 09:59:52 +0200

    chore: improve readability

--- src/sql/accounts/accounts_stake_address_transactions.sql
+++ src/sql/accounts/accounts_stake_address_transactions.sql
@@ -1,114 +1,100 @@
-SELECT encode(unique_txs.hash, 'hex') AS "tx_hash",
-  unique_txs.block_index AS "tx_index",
-  unique_txs.block_no AS "block_height",
-  extract(
-    epoch
-    FROM unique_txs.time
-  )::INTEGER AS "block_time"
-FROM (
+WITH candidate_tx_ids AS (
+  SELECT txi.tx_in_id AS tx_id
+  FROM stake_address sa
+    JOIN tx_out txo ON txo.stake_address_id = sa.id
+    JOIN tx_in txi ON txi.tx_out_id = txo.tx_id AND txi.tx_out_index = txo.index
+    JOIN tx ON tx.id = txi.tx_in_id
+    JOIN block b ON b.id = tx.block_id
+  WHERE sa.view = $4
+    AND (
     (
-      SELECT tx.id,
-        tx.hash,
-        tx.block_index,
-        b.block_no,
-        b.time
-      FROM tx
-        JOIN tx_in txi ON (txi.tx_in_id = tx.id)
-        JOIN tx_out txo ON (
-          txo.tx_id = txi.tx_out_id
-          AND txo.index = txi.tx_out_index
-        )
-        JOIN stake_address sa ON (sa.id = txo.stake_address_id)
-        JOIN block b ON (b.id = tx.block_id)
-      WHERE sa.view = $4
-        AND (
-          (
-            -- :: cast of parameters is necessary for PG in order to validate against NULL
-            $5::INTEGER IS NULL
-            OR b.block_no > $5
-          )
-          OR (
-            (
-              $6::INTEGER IS NULL
-              AND b.block_no = $5
-            )
-            OR (
-              tx.block_index >= $6
-              AND b.block_no = $5
-            )
-          )
-        )
-        AND (
-          (
-            $7::INTEGER IS NULL
-            OR b.block_no < $7
-          )
-          OR (
-            (
-              $8::INTEGER IS NULL
-              AND b.block_no = $7
-            )
-            OR (
-              tx.block_index <= $8
-              AND b.block_no = $7
-            )
-          )
-        )
+      -- :: cast of parameters is necessary for PG in order to validate against NULL
+      $5::INTEGER IS NULL
+      OR b.block_no > $5
     )
-    UNION
-    -- remove duplicate from SELF TXs via UNION
+    OR (
+      (
+        $6::INTEGER IS NULL
+        AND b.block_no = $5
+      )
+      OR (
+        tx.block_index >= $6
+        AND b.block_no = $5
+      )
+    )
+  )
+  AND (
     (
-      SELECT tx.id,
-        tx.hash,
-        tx.block_index,
-        b.block_no,
-        b.time
-      FROM tx
-        JOIN tx_out txo ON (txo.tx_id = tx.id)
-        JOIN stake_address sa ON (sa.id = txo.stake_address_id)
-        JOIN block b ON (b.id = tx.block_id)
-      WHERE sa.view = $4
-        AND (
-          (
-            -- :: cast of parameters is necessary for PG in order to validate against NULL
-            $5::INTEGER IS NULL
-            OR b.block_no > $5
-          )
-          OR (
-            (
-              $6::INTEGER IS NULL
-              AND b.block_no = $5
-            )
-            OR (
-              tx.block_index >= $6
-              AND b.block_no = $5
-            )
-          )
-        )
-        AND (
-          (
-            $7::INTEGER IS NULL
-            OR b.block_no < $7
-          )
-          OR (
-            (
-              $8::INTEGER IS NULL
-              AND b.block_no = $7
-            )
-            OR (
-              tx.block_index <= $8
-              AND b.block_no = $7
-            )
-          )
-        )
+      $7::INTEGER IS NULL
+      OR b.block_no < $7
     )
-  ) AS "unique_txs"
+    OR (
+      (
+        $8::INTEGER IS NULL
+        AND b.block_no = $7
+      )
+      OR (
+        tx.block_index <= $8
+        AND b.block_no = $7
+      )
+    )
+  )
+
+  UNION
+  SELECT tx.id AS tx_id
+  FROM stake_address sa
+    JOIN tx_out txo ON txo.stake_address_id = sa.id
+    JOIN tx ON tx.id = txo.tx_id
+    JOIN block b ON b.id = tx.block_id
+  WHERE sa.view = $4
+    AND (
+      (
+        -- :: cast of parameters is necessary for PG in order to validate against NULL
+        $5::INTEGER IS NULL
+        OR b.block_no > $5
+      )
+      OR (
+        (
+          $6::INTEGER IS NULL
+          AND b.block_no = $5
+        )
+        OR (
+          tx.block_index >= $6
+          AND b.block_no = $5
+        )
+      )
+    )
+    AND (
+      (
+        $7::INTEGER IS NULL
+        OR b.block_no < $7
+      )
+      OR (
+        (
+          $8::INTEGER IS NULL
+          AND b.block_no = $7
+        )
+        OR (
+          tx.block_index <= $8
+          AND b.block_no = $7
+        )
+      )
+    )
+)
+SELECT
+  encode(tx.hash, 'hex')               AS "tx_hash",
+  tx.block_index                       AS "tx_index",
+  b.block_no                           AS "block_height",
+  EXTRACT(EPOCH FROM b.time)::INTEGER  AS "block_time"
+FROM candidate_tx_ids c
+JOIN tx ON tx.id = c.tx_id
+JOIN block b ON b.id = tx.block_id
 ORDER BY CASE
-    WHEN LOWER($1) = 'desc' THEN unique_txs.id
+    WHEN LOWER($1) = 'desc' THEN tx.id
   END DESC,
   CASE
     WHEN LOWER($1) <> 'desc'
-    OR $1 IS NULL THEN unique_txs.id
+    OR $1 IS NULL THEN tx.id
   END ASC
 LIMIT CASE
     WHEN $2 >= 1
