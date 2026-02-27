cf7c5b53aea13d88da28804b30e827a9f311fdc9
slowbackspace <slowbackspace@gmail.com>
2026-02-27 10:50:25 +0100

    feat: add tx_slot and block_time to /accounts/:sa/*

--- src/sql/accounts/unpaged/accounts_stake_address_mirs.sql
+++ src/sql/accounts/unpaged/accounts_stake_address_mirs.sql
@@ -1,20 +1,28 @@
 SELECT encode(tx_hash, 'hex') AS "tx_hash",
-  amount::TEXT AS "amount" -- cast to TEXT to avoid number overflow
+  amount::TEXT AS "amount", -- cast to TEXT to avoid number overflow
+  tx_slot AS "tx_slot",
+  block_time AS "block_time"
 FROM(
     SELECT tx.id AS "id",
       tx.hash AS "tx_hash",
-      r.amount AS "amount"
+      r.amount AS "amount",
+      b.slot_no::INTEGER AS "tx_slot",
+      EXTRACT(EPOCH FROM b.time)::INTEGER AS "block_time"
     FROM tx
       JOIN reserve r ON (tx.id = r.tx_id)
       JOIN stake_address sa ON (sa.id = r.addr_id)
+      JOIN block b ON (b.id = tx.block_id)
     WHERE sa.view = $2
     UNION ALL
     SELECT tx.id AS "id",
       tx.hash AS "tx_hash",
-      t.amount AS "amount"
+      t.amount AS "amount",
+      b.slot_no::INTEGER AS "tx_slot",
+      EXTRACT(EPOCH FROM b.time)::INTEGER AS "block_time"
     FROM tx
       JOIN treasury t ON (tx.id = t.tx_id)
       JOIN stake_address sa ON (sa.id = t.addr_id)
+      JOIN block b ON (b.id = tx.block_id)
     WHERE sa.view = $2
   ) AS "unordered_txs"
 ORDER BY CASE
--- src/sql/accounts/unpaged/accounts_stake_address_withdrawals.sql
+++ src/sql/accounts/unpaged/accounts_stake_address_withdrawals.sql
@@ -1,12 +1,17 @@
 SELECT encode(tx_hash, 'hex') AS "tx_hash",
-  amount::TEXT AS "amount" -- cast to TEXT to avoid number overflow
+  amount::TEXT AS "amount", -- cast to TEXT to avoid number overflow
+  tx_slot AS "tx_slot",
+  block_time AS "block_time"
 FROM(
     SELECT tx.id AS "id",
       tx.hash AS "tx_hash",
-      w.amount AS "amount"
+      w.amount AS "amount",
+      b.slot_no::INTEGER AS "tx_slot",
+      EXTRACT(EPOCH FROM b.time)::INTEGER AS "block_time"
     FROM tx
       JOIN withdrawal w ON (tx.id = w.tx_id)
       JOIN stake_address sa ON (sa.id = w.addr_id)
+      JOIN block b ON (b.id = tx.block_id)
     WHERE sa.view = $2
   ) AS "unordered_txs"
 ORDER BY CASE
